fa5d75f714e64db50478d4c951f035ec1c07b682
inoabrian <brianmust1@gmail.com>
2019-02-10 14:50:48 -0500

    updated service to user a separate method for querying and added comments

--- parkingspot_service/index.ts
+++ parkingspot_service/index.ts
@@ -6,19 +6,28 @@ import * as mongo from 'mongodb';
 import * as db from "./database";
 import { IncomingMessage, ServerResponse } from "http";
 
+// method to query db for any parking spots a user owns or are available
+const getAvailableSpots: (connection: mongo.MongoClient, userId: mongo.ObjectID) => Promise<any[]> = async (connection: mongo.MongoClient, userId: mongo.ObjectID): Promise<any[]> => {
+    let result: Promise<any[]> = connection.db()
+            .collection(process.env.MONGO_COLLECTION)
+            .find({owner: userId})
+            .toArray();
+}
+
 const list: router.AugmentedRequestHandler = async (request: router.ServerRequest, response: ServerResponse): Promise<any> => {
+    // mongo db connection promise
     let dbConnection: () => Promise<mongo.MongoClient> = db.default;
 
     let connection: mongo.MongoClient = await dbConnection();
 
+    // get userId from query params
     let userId = request.params.userId ? new mongo.ObjectID(request.params.userId) : null;
 
-    let result: mongo.Cursor = connection.db()
-            .collection(process.env.MONGO_COLLECTION)
-            .find({owner: userId});
+    // get available spots that no one owns/reserved/booked
+    let availableParkingSpots: any[] = await getAvailableSpots(connection, userId);
 
-    if(result)
-        micro.send(response, 200, await result.toArray());
+    if(availableParkingSpots.length)
+        micro.send(response, 200, availableParkingSpots);
     else     
         micro.sendError(request, response, {statusCode: 304, message: 'Could not find any spots'});   
 };

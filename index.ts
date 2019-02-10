30e9d3bf4102aa1d1f8cf6e89c1d0875b163cbd1
inoabrian <brianmust1@gmail.com>
2019-02-10 15:58:12 -0500

    expose all of the parking spot book service functionality

---
+++ index.ts
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+import * as micro from "micro";
+import * as router from "microrouter";
+
+import * as parking_service from "./parkingspot_service/index";
+import * as reserve_service from "./reserve_service/index";
+import * as booking_service from "./booking_service/index";
+
+const service: micro.RequestHandler = router.router(
+    ...reserve_service,
+    ...parking_service,
+    ...booking_service
+);
+
+export = service;

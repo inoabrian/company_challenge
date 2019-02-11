0fd16a2d002c74904a36b7d00c7b8035f3135c41
inoabrian <brianmust1@gmail.com>
2019-02-10 23:29:10 -0500

    cleaned up default data, deleted .env

--- parkingspot_service/database.ts
+++ parkingspot_service/database.ts
@@ -2,5 +2,5 @@ require('dotenv').config();
 import * as mongo from 'mongodb';
 
 export default (): Promise<mongo.MongoClient> => {
-  return mongo.connect(process.env.MONGO_URL);
+  return mongo.connect(process.env.MONGO_URL, { useNewUrlParser: true });
 };
--- parkingspot_service/index.ts
+++ parkingspot_service/index.ts
@@ -19,8 +19,14 @@ const getAvailableSpots: (connection: mongo.MongoClient, userId: mongo.ObjectID)
 const list: router.AugmentedRequestHandler = async (request: router.ServerRequest, response: ServerResponse): Promise<any> => {
     // mongo db connection promise
     let dbConnection: () => Promise<mongo.MongoClient> = db.default;
+    let connection: mongo.MongoClient;
 
-    let connection: mongo.MongoClient = await dbConnection();
+    try {
+        connection = await dbConnection();
+    }
+    catch(ex){
+        console.error(ex);
+    }
 
     // get userId from query params
     let userId = request.params.userId ? new mongo.ObjectID(request.params.userId) : null;
@@ -28,15 +34,41 @@ const list: router.AugmentedRequestHandler = async (request: router.ServerReques
     // get available spots that no one owns/reserved/booked
     let availableParkingSpots: any[] = await getAvailableSpots(connection, userId);
 
-    if(availableParkingSpots.length)
-        micro.send(response, 200, availableParkingSpots);
-    else     
-        micro.sendError(request, response, {statusCode: 304, message: 'Could not find any spots'});   
+    micro.send(response, 200, availableParkingSpots);
 };
 
-const service: micro.RequestHandler[] =[ 
+const create: router.AugmentedRequestHandler = async(request: router.ServerRequest, response: router.ServerResponse): Promise<any> => {
+    // mongo db connection promise
+    let dbConnection: () => Promise<mongo.MongoClient> = db.default;
+
+    let connection: mongo.MongoClient;
+
+    try {
+        connection = await dbConnection();
+    }
+    catch(ex){
+        console.error(ex);
+        micro.send(response, 500);
+    }
+
+    let created: mongo.InsertWriteOpResult = await connection.db()
+        .collection(process.env.MONGO_COLLECTION)
+        .insertMany(
+            [
+                {"lat": 40.8135923, "lon": -74.2177845, "zone": "1A", "street": "S. Fullterton Avenue", "owner": null},
+                {"lat": 40.813540, "lon":  -74.217724, "zone": "1A", "street": "S. Fullterton Avenue", "owner": null},
+                {"lat": 40.813333, "lon": -74.217798, "zone": "1A", "street": "S. Fullterton Avenue", "owner": null},
+                {"lat": 40.812939, "lon": -74.217891, "zone": "1A", "street": "S. Fullterton Avenue", "owner": null}
+            ]
+        );
+
+    micro.send(response, 200, created);
+};
+
+const service: micro.RequestHandler[] = [ 
     router.get('/list', list),
-    router.get('/listUser/:userId', list)
+    router.get('/listUser/:userId', list),
+    router.post('/seed_list', create)
 ];
 
 export = service;
